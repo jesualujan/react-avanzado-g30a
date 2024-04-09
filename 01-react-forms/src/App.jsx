c9a6d5939eab5fe557d59eb4a0e95effb04fa0d8
jesualujan <lujan.oficial@gmail.com>
2024-04-08 20:01:54 -0600

    ✨ importar mis componentes

--- 01-react-forms/src/App.jsx
+++ 01-react-forms/src/App.jsx
@@ -1,10 +1,13 @@
+import ReactHookForm from '../Components/ReactHookForm/ReactHookForm'
+import SimpleForm from '../Components/SimpleForm/SimpleForm'
 import './App.css'
 
+
 function App() {
- 
   return (
     <>
-     
+     <ReactHookForm />
+     <SimpleForm />
     </>
   )
 }
