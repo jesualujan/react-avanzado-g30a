fc2c2461e9a418ffba8b57c94aa79bef939be07c
jesualujan <lujan.oficial@gmail.com>
2024-04-08 20:02:32 -0600

    ✨ crear el template de los formularios

--- 01-react-forms/Components/ReactHookForm/ReactHookForm.jsx
+++ 01-react-forms/Components/ReactHookForm/ReactHookForm.jsx
@@ -1,7 +1,80 @@
-
+import logo from './logo.svg'
 const ReactHookForm = () => {
   return (
-    <div>ReactHookForm</div>
+    //* VA TODO NUESTRO DISEÑO HTML
+    //? Paso #1: Crear el formulario base en JSX
+    <div className='login'>
+    <div className='login-container'>
+      <img src={logo} alt='logo' />
+      <form
+      
+        style={{ display: 'flex', flexDirection: 'column' }}
+      >
+      
+        <label htmlFor='firstName'>Nombre</label>
+        <input
+          type='text'
+          name='firstName'
+          placeholder='Tu Nombre'
+          id='firstName'
+         
+        />
+        <p></p>
+
+        <label htmlFor='lastName'>Apellido</label>
+        <input
+          type='text'
+          name='lastName'
+          placeholder='Tu Apellido'
+          id='lastName'
+          
+        />
+        <p></p>
+
+        <label htmlFor='age'>Edad</label>
+        <input
+          type='number'
+          name='age'
+          placeholder='Tu Edad'
+          id='age'
+       
+        />
+        <p>{/* */}</p>
+
+        <label htmlFor='gender'>Genero</label>
+        <select name='gender' id='gender'>
+          <option value=''>Elige un genero</option>
+          <option value='M'>Masculino</option>
+          <option value='F'>Femenino</option>
+          <option value='O'>Otro</option>
+        </select>
+        <p></p>
+
+        <label htmlFor='email'>Email</label>
+        <input
+          type='text'
+          name='email'
+          placeholder='correo@mail.com'
+          id='email'
+      
+        />
+        <p></p>
+
+        <label htmlFor='password'>Password</label>
+        <input
+          type='password'
+          name='password'
+          id='password'
+        
+        />
+        <p>{/* */}</p>
+
+        <button type='submit'>
+          Iniciar Sesion
+        </button>
+      </form>
+    </div>
+  </div>
   )
 }
 
--- 01-react-forms/Components/SimpleForm/SimpleForm.jsx
+++ 01-react-forms/Components/SimpleForm/SimpleForm.jsx
@@ -1,8 +1,41 @@
-
+import logo from './logo.svg'
 
 const SimpleForm = () => {
   return (
-    <div>SimpleForm</div>
+   //* VA TODO NUESTRO DISEÑO HTML
+    //? Paso #1: Crear el formulario base en JSX
+    <div className='login'>
+      <div className='login-container'>
+        <img src={logo} alt='logo' />
+        
+        <form >
+            {/* Paso #3: Guardo la información en el estado de */}
+          <label htmlFor='email'>Email</label>
+          <input
+            type='text'
+            name='email'
+            placeholder='correo@mail.com'
+            id='simple-email'
+           
+           
+          />
+
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            name='password'
+            id='simple-password'
+    
+        
+          />
+          
+          <button type='submit'>
+            Iniciar Sesion
+          </button>
+          
+        </form>
+      </div>
+    </div>
   )
 }
 
