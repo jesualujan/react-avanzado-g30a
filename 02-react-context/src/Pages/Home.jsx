415b952f10df9dce33724fbc2b0083fc109c515e
jesualujan <lujan.oficial@gmail.com>
2024-04-10 21:44:49 -0600

    ✨ importamos nuestros componentes en home

--- 02-react-context/src/Pages/Home.jsx
+++ 02-react-context/src/Pages/Home.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import SongList from '@/Components/SongList/SongList'
+import SongDetail from '@/Components/SongDetail/SongDetail'
+import { SongProvider } from '@/Context/SongContex'
+import './home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <SongProvider>
+      <div className='home-container'>
+        <div className='left'>
+          <p>lado zquierdo</p>
+          <SongList />
+        </div>
+        <div className='right'>
+          <p>lado Derecho</p>
+          <SongDetail />
+        </div>
+      </div>
+    </SongProvider>
   )
 }
 
