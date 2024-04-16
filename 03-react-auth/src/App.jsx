df2720522420ac91f3cf32b46db17020ef93c551
jesualujan <lujan.oficial@gmail.com>
2024-04-15 19:59:43 -0600

    🎉 creación del proyecto

---
+++ 03-react-auth/src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App () {
+  return (
+    <>
+      <h1> REACT AUTH </h1>
+    </>
+  )
+}
+
+export default App
---
+++ 03-react-auth/src/Pages/index.js
@@ -0,0 +1,14 @@
+import Dashboard from './Dashboard'
+import Home from './Home'
+import Login from './Login'
+import Secret from './Secret'
+import Signup from './Signup'
+
+// EPORTAR COMO UN OBJETO
+export {
+  Dashboard,
+  Home,
+  Login,
+  Secret,
+  Signup
+}
