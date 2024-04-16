4f21dc23d0d8a012acc336c8a3514a6ac51e4125
jesualujan <lujan.oficial@gmail.com>
2024-04-15 21:08:48 -0600

    ✨ Aplicando BEM en el html

--- 03-react-auth/src/Components/Header.jsx
+++ 03-react-auth/src/Components/Header.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import './header.scss'
 
 const Header = () => {
   return (
-    <div>Header</div>
+  // BEM PARA NUESTRAS CLASES EN HTML
+  // kebabcase para nombrar nuestras className en React con minúsculas
+    <nav className='header'> {/*  BLOQUE */}
+      <a className='header__logo' href='/'>LOGO</a>
+      <ul className='header__nav-list' />
+      <li className='header__list-item'>
+        <a className='header__item-link header__item-link--is-active' href='/home'>Home</a>
+      </li>
+      <li className='header__list-item'>
+        <a className='header__item-link' href='/dashboard'>Dashboard</a>
+      </li>
+      <li className='header__list-item'>
+        <a className='header__item-link' href='/secret'>Secret</a>
+      </li>
+      <li className='header__list-item'>
+        <a className='header__item-link' href='/login'>Login</a>
+      </li>
+      <li className='header__list-item'>
+        <a className='header__item-link' href='/signup'>Signup</a>
+      </li>
+    </nav>
   )
 }
 
