9b7624f1596e44cf35e810b7e98e1e42635c1fa1
jesualujan <lujan.oficial@gmail.com>
2024-04-17 21:28:08 -0600

    ✨ llamada a la API apartado usuarios

---
+++ 03-react-auth/src/Service/useService.js
@@ -0,0 +1,24 @@
+// AQUÍ HACEMOS LA LLAMADA A LA API Y USAMOS AXIOS
+// LE PASAMOS UN ENDPOINT PARA QUE TRAIGA LA DATA DE LA API
+
+//* 1) IMPORTAMOS AXIOS
+import axios from 'axios'
+
+//* 2) ESTABLECEMOS NUESTRO ENDPOINT
+//  SNAKE_UPPERCASE -> para entornos o variables globales
+
+const BASE_URL = 'http://localhost:3000' // esta la podemos cambiar si subimos el repo de github a render
+
+// ocupamos mandar información
+// registrar un usuario
+const registerUserService = (data) => axios.post(`${BASE_URL}/register`, data)
+
+// login
+const loginUserService = (data) => axios.post(`${BASE_URL}/login`, data)
+
+// información de un solo usuario
+
+export {
+  registerUserService,
+  loginUserService
+}
