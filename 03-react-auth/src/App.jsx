1c9e0732bd88e902b5a3b45eed88f36b9e737ff8
jesualujan <lujan.oficial@gmail.com>
2024-04-17 20:34:12 -0600

    ✨ importo el componente BrowserRouter y mis Rutas

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -1,10 +1,15 @@
 import './App.css'
 import Header from '@/Components/Header'
+import { BrowserRouter } from 'react-router-dom'
+import RoutesIndex from '@/Routes/RoutesIndex'
 
 function App () {
   return (
     <>
-      <Header />
+      <BrowserRouter>
+        <Header />
+        <RoutesIndex />
+      </BrowserRouter>
     </>
   )
 }
