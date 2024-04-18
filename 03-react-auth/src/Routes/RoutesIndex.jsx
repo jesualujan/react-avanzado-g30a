b9465fcea1326372b3a49714e025c005c64f6c2f
jesualujan <lujan.oficial@gmail.com>
2024-04-17 20:14:32 -0600

    ✨ Creamos nuestras rutas

---
+++ 03-react-auth/src/Routes/RoutesIndex.jsx
@@ -0,0 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import { Home, Dashboard, Login, Secret, Signup } from '@/Pages'
+
+// PascalCase para nombrar nuestros componentes
+const RoutesIndex = () => {
+  return (
+    <Routes>
+      <Route path='/' element={<Home />} />
+      <Route path='/dashboard' element={<Dashboard />} />
+      <Route path='/login' element={<Login />} />
+      <Route path='/secret' element={<Secret />} />
+      <Route path='/signup' element={<Signup />} />
+    </Routes>
+  )
+}
+
+export default RoutesIndex
