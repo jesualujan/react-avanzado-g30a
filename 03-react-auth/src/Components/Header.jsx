0da87e8b7ced3232381e30eef89d5ac3ee885cc7
jesualujan <lujan.oficial@gmail.com>
2024-04-17 20:14:50 -0600

    ✨ utilizamos NavLink

--- 03-react-auth/src/Components/Header.jsx
+++ 03-react-auth/src/Components/Header.jsx
@@ -1,26 +1,29 @@
+import { NavLink } from 'react-router-dom'
 import './header.scss'
 
 const Header = () => {
+  // camelCase para las funciones, métodos o variables del componente
+  const linkIsActive = (isActive) => isActive ? 'header__item-link header__item-link--is-active' : 'header__item-link'
   return (
   // BEM PARA NUESTRAS CLASES EN HTML
   // kebabcase para nombrar nuestras className en React con minúsculas
     <nav className='header'> {/*  BLOQUE */}
-      <a className='header__logo' href='/'>LOGO</a>
+      <NavLink className='header__logo' to='/'> LOGO </NavLink>
       <ul className='header__nav-list' />
       <li className='header__list-item'>
-        <a className='header__item-link header__item-link--is-active' href='/home'>Home</a>
+        <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/home'>Home</NavLink>
       </li>
       <li className='header__list-item'>
-        <a className='header__item-link' href='/dashboard'>Dashboard</a>
+        <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/dashboard'>Dashboard</NavLink>
       </li>
       <li className='header__list-item'>
-        <a className='header__item-link' href='/secret'>Secret</a>
+        <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/secret'>Secret</NavLink>
       </li>
       <li className='header__list-item'>
-        <a className='header__item-link' href='/login'>Login</a>
+        <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/login'>Login</NavLink>
       </li>
       <li className='header__list-item'>
-        <a className='header__item-link' href='/signup'>Signup</a>
+        <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/signup'>Signup</NavLink>
       </li>
     </nav>
   )
