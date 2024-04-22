b77ae9edc35f1cd2659a107fe0d859c8d97beb33
jesualujan <lujan.oficial@gmail.com>
2024-04-22 14:34:37 -0600

    ✨ agregue la etiqueta ul para corrección de estilos

--- 03-react-auth/src/Components/Header.jsx
+++ 03-react-auth/src/Components/Header.jsx
@@ -8,23 +8,24 @@ const Header = () => {
   // BEM PARA NUESTRAS CLASES EN HTML
   // kebabcase para nombrar nuestras className en React con minúsculas
     <nav className='header'> {/*  BLOQUE */}
-      <NavLink className='header__logo' to='/'> LOGO </NavLink>
-      <ul className='header__nav-list' />
-      <li className='header__list-item'>
-        <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/home'>Home</NavLink>
-      </li>
-      <li className='header__list-item'>
-        <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/dashboard'>Dashboard</NavLink>
-      </li>
-      <li className='header__list-item'>
-        <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/secret'>Secret</NavLink>
-      </li>
-      <li className='header__list-item'>
-        <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/login'>Login</NavLink>
-      </li>
-      <li className='header__list-item'>
-        <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/signup'>Signup</NavLink>
-      </li>
+      <NavLink className='header__logo' to='/'>LOGO</NavLink>
+      <ul className='header__nav-list'>
+        <li className='header__list-item'>
+          <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/'>Home</NavLink>
+        </li>
+        <li className='header__list-item'>
+          <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/dashboard'>Dashboard</NavLink>
+        </li>
+        <li className='header__list-item'>
+          <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/secret'>Secret</NavLink>
+        </li>
+        <li className='header__list-item'>
+          <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/login'>Login</NavLink>
+        </li>
+        <li className='header__list-item'>
+          <NavLink className={({ isActive }) => linkIsActive(isActive)} to='/signup'>Signup</NavLink>
+        </li>
+      </ul>
     </nav>
   )
 }
