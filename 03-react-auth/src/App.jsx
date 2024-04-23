b0053d4092f1aee105d7ca916d1d70f74d9415e3
jesualujan <lujan.oficial@gmail.com>
2024-04-23 14:23:47 -0600

    📝 agregue notitas

--- 03-react-auth/src/App.jsx
+++ 03-react-auth/src/App.jsx
@@ -6,6 +6,7 @@ import { AuthProvider } from '@/Context/AuthContext'
 
 function App () {
   return (
+  // configurar el router
     <>
       <AuthProvider>
         <BrowserRouter>
@@ -18,3 +19,11 @@ function App () {
 }
 
 export default App
+
+/*
+BrowserRouter es un componente
+principal en el enrutamiento en
+aplicaciones React. Es el encargado
+de mantener y actualizar la ubicación
+actual en el navegador, y renderizar
+los componentes asociados a cada ruta. */
