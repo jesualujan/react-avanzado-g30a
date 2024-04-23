225d98e575fbcc3b7d35f4a83c740c1257c8c0ff
jesualujan <lujan.oficial@gmail.com>
2024-04-23 14:27:10 -0600

    ✨ creamos el perfil para usuario admin o customer

--- 03-react-auth/src/Pages/Secret.jsx
+++ 03-react-auth/src/Pages/Secret.jsx
@@ -1,8 +1,18 @@
-import React from 'react'
+import { useAuthContext } from '@/Hook/useAuthContext'
 
 const Secret = () => {
+  const { userPayload } = useAuthContext()
   return (
-    <div>Secret</div>
+    <>
+      <h1> Secret 🙍 </h1>
+      {
+      userPayload?.role === 'ADMIN'
+        ? <h2> Hola Admin! bienvenido ✋</h2>
+        : <h2> Hola Customer! bienvenido ✋  </h2>
+    }
+      {userPayload?.role === 'ADMIN' && <h3> SALUDOS ADMIN </h3>}
+      {userPayload?.role === 'CUSTOMER' && <h3> SALUDOS CUSTOMER</h3>}
+    </>
   )
 }
 
